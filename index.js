7ec8bf6ea874d0f2ccdb342916bd54e5ee20c4e2
Aseiide <eiseiide@gmail.com>
2021-11-08 14:29:53 +0900

    wip

--- index.js
+++ index.js
@@ -2,32 +2,33 @@ const {
   Select
 } = require("enquirer");
 
+async function sample() {
+  const prompt = new Select({
+    name: 'cups',
+    message: 'あなたは1日に何杯のコーヒーを飲みますか',
+    choices: ['1杯', '2杯', '3杯']
+  });
 
-const prompt = new Select({
-  name: 'cups',
-  message: 'あなたは1日に何杯のコーヒーを飲みますか',
-  choices: ['1杯', '2杯', '3杯']
-});
+  await prompt
+    .run()
+    .then(answer => console.log(`${answer}ですね`))
+    .catch(console.error);
 
-// 実行完了待ち
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-prompt
-  .run()
-  .then(answer => console.log(`${answer}ですね`))
-  .catch(console.error);
+  readline.question('1回あたりに使うコーヒー豆の量をグラムで入力してください\n', (answer) => {
+    console.log(`${answer}gですね`);
+    readline.close();
+  })
 
-// 実行完了待ち
+  function calc(cups, grams) {
+    amount = cups * grams
+    console.log(`一ヶ月に必要なコーヒーの量は約${amount}gです`)
+  }
+}
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// 実行完了待ち
-
-readline.question('1回あたりに使うコーヒー豆の量をグラムで入力してください', (answer) => {
-  console.log(`${answer}gですね`);
-  readline.close();
-})
-
-// 実行完了待ち
+sample()
+calc(cups, grams)
