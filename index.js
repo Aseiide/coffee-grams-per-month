1a46b87dd0a1306d3c4c458f8fbac5321c2a51d6
Aseiide <eiseiide@gmail.com>
2021-11-08 12:48:15 +0900

    WIP

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const { prompt } = require("enquirer");
+const {
+  Select
+} = require("enquirer");
 
 
-const question = {
-  type: 'input',
-  name: "cups",
-  message: "あなたは1日に何杯コーヒーを飲みますか"
-};
+const prompt = new Select({
+  name: 'cups',
+  message: 'あなたは1日に何杯のコーヒーを飲みますか',
+  choices: ['1杯', '2杯', '3杯']
+});
 
-prompt(question)
-  .then(answer => console.log('Answer', answer))
+// 実行完了待ち
+
+prompt
+  .run()
+  .then(answer => console.log(`${answer}ですね`))
   .catch(console.error);
+
+// 実行完了待ち
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// 実行完了待ち
+
+readline.question('1回あたりに使うコーヒー豆の量をグラムで入力してください', (answer) => {
+  console.log(`${answer}gですね`);
+  readline.close();
+})
+
+// 実行完了待ち
