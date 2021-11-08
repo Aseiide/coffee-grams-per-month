b30a89069537762bbd541fcb92d329b3b2ac5ce7
Aseiide <eiseiide@gmail.com>
2021-11-08 15:41:05 +0900

    wip

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ async function sample() {
     }
   ]);
 
-  amount = response.cups * response.grams
-  console.log(`1ヶ月に必要なコーヒー豆の量は約${amount}gです`)
+  if (cups > 10) {
+    console.log('コーヒーの飲みすぎです！デカフェも試して量を減らしてみましょう。')
+  } else if (4 < cups && cups < 10) {
+    console.log('コーヒーの飲みすぎは身体によくありません。もう少し抑えて見てください')
+  } else if (cups < 0) {
+    console.log('ありえない数値です。正の整数を入力してください')
+  } else {
+    cups = response.cups
+  }
+  //   grams = response.grams
+  //   amount = cups * grams
+  //   console.log(`1ヶ月に必要なコーヒー豆の量は約${amount}gです`)
 }
 
 sample();
