f68cca94c059328d82fe6db26df8f3aa8569fbe1
Aseiide <eiseiide@gmail.com>
2021-11-08 15:03:22 +0900

    大枠が完成

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const {
-  Select
-} = require("enquirer");
+  prompt
+} = require('enquirer');
 
 async function sample() {
-  const prompt = new Select({
-    name: 'cups',
-    message: 'あなたは1日に何杯のコーヒーを飲みますか',
-    choices: ['1杯', '2杯', '3杯']
-  });
+  const response = await prompt([{
+      type: 'input',
+      name: 'cups',
+      message: 'あなたは1日に何杯のコーヒーを飲みますか。数字を入力してください'
+    },
+    {
+      type: 'input',
+      name: 'grams',
+      message: '1回あたりに使うコーヒー豆の量をグラム数で入力してください'
+    }
+  ]);
 
-  await prompt
-    .run()
-    .then(answer => console.log(`${answer}ですね`))
-    .catch(console.error);
-
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  readline.question('1回あたりに使うコーヒー豆の量をグラムで入力してください\n', (answer) => {
-    console.log(`${answer}gですね`);
-    readline.close();
-  })
-
-  function calc(cups, grams) {
-    amount = cups * grams
-    console.log(`一ヶ月に必要なコーヒーの量は約${amount}gです`)
-  }
+  amount = response.cups * response.grams
+  console.log(`1ヶ月に必要なコーヒー豆の量は約${amount}gです`)
 }
 
-sample()
-calc(cups, grams)
+sample();
