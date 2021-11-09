cab35bc3faa18287aa5def3162d6dc93769b5672
Aseiide <eiseiide@gmail.com>
2021-11-09 11:19:27 +0900

    cliの大枠が完成

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-const {
-  prompt
-} = require('enquirer');
+async function cups() {
+  const {
+    Select
+  } = require('enquirer');
 
-async function sample() {
-  const response = await prompt([{
-      type: 'input',
-      name: 'cups',
-      message: 'あなたは1日に何杯のコーヒーを飲みますか。数字を入力してください'
-    },
-    {
-      type: 'input',
-      name: 'grams',
-      message: '1回あたりに使うコーヒー豆の量をグラム数で入力してください'
-    }
-  ]);
+  const response = new Select({
+    name: 'cups',
+    message: '1日に何杯コーヒーを飲みますか?',
+    choices: ['1', '2', '3']
+  });
 
-  if (cups > 10) {
-    console.log('コーヒーの飲みすぎです！デカフェも試して量を減らしてみましょう。')
-  } else if (4 < cups && cups < 10) {
-    console.log('コーヒーの飲みすぎは身体によくありません。もう少し抑えて見てください')
-  } else if (cups < 0) {
-    console.log('ありえない数値です。正の整数を入力してください')
-  } else {
-    cups = response.cups
-  }
-  //   grams = response.grams
-  //   amount = cups * grams
-  //   console.log(`1ヶ月に必要なコーヒー豆の量は約${amount}gです`)
+  await response
+    .run()
+    .then(answer => console.log(`${answer}杯ですね`))
+    .catch(console.error)
+
+  cups = response.value
+  console.log(cups)
+
+  const {
+    prompt
+  } = require('enquirer');
+
+  const res = await prompt({
+    type: 'input',
+    name: 'grams',
+    message: '1回あたりに使うコーヒーの量を入力してください',
+    initial: 'g'
+  });
+
+  grams = res.grams
+  console.log(`${grams}gですね`)
+
+  amount = cups * grams
+  console.log(`1ヶ月に必要なコーヒーの量は約${amount}gです`)
 }
 
-sample();
+cups();
+
+
+// async function calc(cups, grams) {
+//   cups();
+//   grams();
+//   amount = cups * grams
+//   await console.log(`1ヶ月に必要なコーヒーの量は${amount}gです`)
+// }
+
+// calc(cups, grams)
