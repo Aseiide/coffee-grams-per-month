16c2b22a69ec35674ab5dedbd489da3c418579ed
Aseiide <eiseiide@gmail.com>
2021-11-10 17:02:39 +0900

    頻度なしで計算するnpmを実装

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ async function main () {
     message: '1日に何杯コーヒーを飲みますか?',
     choices: ['1', '2', '3', '4']
   })
-  
-  const cups = choices.value
 
   await choices
     .run()
-    .then((cups) => {
+    .then(getNumberOfCups = (cups) => {
       if (cups === '4') {
         console.log('コーヒーの飲みすぎはよくありませんよ！減らしましょう。')
+        return false
       } else {
-        console.log(`${cups}杯ですね`)
-        console.log(cups)
         return cups
       }
     })
@@ -31,55 +28,28 @@ async function main () {
     initial: 'g'
   })
 
-  const grams = res.value
-
   await res
     .run()
-    .then((grams) => {
+    .then(getWeight = (grams) => {
       if (grams < 10) {
         console.log('1回に使うコーヒーの量が少なすぎます。もう一度入力してください')
+        return false
       } else if (grams >= 80) {
         console.log('1回に使うコーヒーの量が多すぎます。もう一度入力してください')
+        return false
       } else {
-        console.log(`${grams}gですね`)
         return grams
       }
     })
     .catch(console.error)
 
-  const {
-    Confirm
-  } = require('enquirer')
+  const quantity = getNumberOfCups(choices.value) // 杯数
+  const grams = getWeight(res.value) // 重さ(g)
+  const amount = quantity * grams * 30
 
-  const prompt = new Confirm({
-    name: 'question',
-    message: 'コーヒーは毎日飲みますか?'
-  })
-
-  const frequency = null
-  const answer = null
-  await prompt
-    .run()
-    .then(answer, frequency => {
-      if (answer) {
-        frequency = 1
-        return frequency
-      } else {
-        frequency = 2
-        return frequency
-      }
-    })
-    .catch(console.error)
-
-
-
-  console.log(cups)
-  console.log(grams)
-  console.log(frequency)
-  const amount = cups * grams * 30 / frequency
-
-  console.log(`1ヶ月に必要なコーヒーの量は約${amount}gです`)
-
+  if (quantity && grams) {
+    console.log(`1ヶ月に必要なコーヒーの量は約${amount}gです`)
+  }
 }
 
 main()
